dedc58811e50d19449ee224fd9e4dc7c28a4ae63
pnakibar <pedronakibar@gmail.com>
2015-07-02 14:42:39 -0300

    utf8 no main

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Pedro Mathias Nakibar - @pnakibar - pedronakibar@gmail.com
+Programa feito para a disciplina de TPA para o curso de Sistemas de Informação do IFES Serra
+Caos não consiga rodar favor ler o README.md e seguir as instruções!
+Lembre-se de instalar o pygame!
+'''
 from model.labirinto import *
 from control.gameControl import *
 import sys
